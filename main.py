0f28ba6f658031fe4b1295ef38ab746b19d6697e
isckRIP <ma.danilovv2001@gmail.com>
2022-10-30 23:58:44 +0300

    Добавлен прокси и поиск ссылок на объявления

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from fake_useragent import UserAgent
 import time
-import fake_useragent
 
+# Create options
+options = webdriver.ChromeOptions()
+# Create user-agent
+useragent = UserAgent()
+
+# Add arguments for options, proxy
+options.add_argument(f"user-agent={useragent.random}")
+# options.add_argument("--proxy-server=80.78.248.167:3128")
+
+# Set url
 url = "https://www.avito.ru/sankt-peterburg/telefony/mobilnye_telefony/samsung-ASgBAgICAkS0wA2crzmwwQ2I_Dc?cd=1&q=samsung"
-driver = webdriver.Chrome(executable_path="C:\\Users\\madan\\PycharmProjects\\AvitoParser\\ChromeDriver\\chromedriver.exe")
+
+driver = webdriver.Chrome(
+    executable_path=r"C:\Users\madan\PycharmProjects\AvitoParser\ChromeDriver\chromedriver.exe",
+    options=options
+)
 
 try:
     driver.get(url=url)
+    print(f"Currently URL is: {driver.current_url}")
+    time.sleep(5)
+
+# Finding links
+    items = driver.find_elements(By.XPATH, "//h3[@itemprop='name']")
+    print(items)
+
+    items[0].click()
     time.sleep(5)
 except Exception as ex:
     print(ex)
